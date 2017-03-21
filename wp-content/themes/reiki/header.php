4ebacfb19de734e38789c7fb80ff495e434e3de5
wercker <Tom.Groskamp@Itonomy.nl>
2017-03-21 11:02:43 +0100

    moved homepage banner around

--- wp-content/themes/reiki/header.php
+++ wp-content/themes/reiki/header.php
@@ -10,23 +10,23 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-  <div class="header" style="background-image:url('<?php esc_url(header_image());?>');">
-    <div class="row border_bottom">
-      <div class="logo_col">
-        <?php reiki_logo(); ?>
-      </div>
-      <div class="main_menu_col">
-        <?php
-        wp_nav_menu(array(
-            'theme_location' => 'primary',
-            'menu_id'        => 'drop_mainmenu',
-            'menu_class' => 'fm2_drop_mainmenu',
-            'container_id' => 'drop_mainmenu_container',
-            'fallback_cb' => 'reiki_nomenu_cb'
-        ));
-        ?>
-      </div>
+  <div class="row border_bottom">
+    <div class="logo_col">
+      <?php reiki_logo(); ?>
     </div>
+    <div class="main_menu_col">
+      <?php
+      wp_nav_menu(array(
+          'theme_location' => 'primary',
+          'menu_id'        => 'drop_mainmenu',
+          'menu_class' => 'fm2_drop_mainmenu',
+          'container_id' => 'drop_mainmenu_container',
+          'fallback_cb' => 'reiki_nomenu_cb'
+      ));
+      ?>
+    </div>
+  </div>
+  <div class="header" style="background-image:url('<?php esc_url(header_image());?>');">
     <div class="gridContainer">
       <div class="header_title">
         <h1 class="heading98"><?php echo reiki_title(); ?></h1>
